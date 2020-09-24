b88bd9df8064ba5e4750c4d00d26ed9463d367f1
super-man1 <71827409+super-man1@users.noreply.github.com>
2020-09-24 20:24:17 +0800

    Add files via upload

---
+++ 计算两数之和.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b = 0;
+	cout << "请输入两个数:" << endl;
+	cin >> a >> b;
+	cout << "这两个数分别是：" << a << "and" << b << "两个数的的和是：" << a + b << endl;
+}
