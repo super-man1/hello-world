add89e720be8ebe380b001a0c8e0e1c2d9210b89
super-man1 <71827409+super-man1@users.noreply.github.com>
2020-09-24 20:27:18 +0800

    Add files via upload

---
+++ for 循环.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	//计算1-100的和
+	int total = 0;
+	for (int i = 0; i <=100; i++)
+	{
+		total += i; //total =i+total
+		cout << i << endl;
+		cout << total << endl;
+	}
+}
