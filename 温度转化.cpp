595e662515836f07b46bade046ca1ccc22477549
super-man1 <71827409+super-man1@users.noreply.github.com>
2020-10-17 12:24:49 +0800

    Add files via upload

---
+++ 温度转化.cpp
@@ -0,0 +1,14 @@
+﻿//华氏温度=1.8*摄氏温度+32
+#include <iostream>
+using namespace std;
+int zhuanhua(int s) //参数就是摄氏度 
+{
+	return  1.8 * s + 32;//返回值  结果
+}
+int main()
+{
+	cout << "请输入一个摄温度：" << endl;
+	int s;//设置一个变量 摄氏度
+	cin >> s;//再让用户生成一个数字（摄氏度）
+	cout << zhuanhua(s) << endl;//调用这个函数
+}
